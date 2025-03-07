5aeb2b8996e09ed1f4fab2126f5f1d596e54cfa6
Fahad <mac@Fahads-Mac-Yeah.local>
2025-03-07 12:57:34 +0530

    Stage-9-Start

--- mynitcbase/Algebra/Algebra.cpp
+++ mynitcbase/Algebra/Algebra.cpp
@@ -160,3 +160,91 @@ int Algebra::insert(char relName[ATTR_SIZE], int nAttrs, char record[][ATTR_SIZE
 
     return retVal;
 }
+int Algebra::select(char srcRel[ATTR_SIZE], char targetRel[ATTR_SIZE], char attr[ATTR_SIZE], int op, char strVal[ATTR_SIZE])
+{
+   
+    int srcRelid=OpenRelTable::getRelId(srcRel);
+    if(srcRelid==E_RELNOTOPEN){
+        return E_RELNOTOPEN;
+    }
+
+   
+    AttrCatEntry attrCatEntry;
+    AttrCacheTable::getAttrCatEntry(srcRelid,attr,&attrCatEntry);
+
+    Attribute attrVal;
+    int type = attrCatEntry.attrType;
+
+    if (type == NUMBER)
+    {
+      
+
+        if(isNumber(strVal))
+        {
+            
+            attrVal.nVal=atof(strVal);
+            
+        }
+        else
+        {
+            return E_ATTRTYPEMISMATCH;
+        }
+    }
+    else if (type == STRING)
+    {
+        
+        strcpy(attrVal.sVal,strVal);
+    }
+
+    
+    RelCatEntry srcrelcatentry;
+    RelCacheTable::getRelCatEntry(srcRelid,&srcrelcatentry);
+    int src_nAttrs =srcrelcatentry.numAttrs;
+
+    char attr_names[src_nAttrs][ATTR_SIZE];
+    int attr_types[src_nAttrs];
+   
+    for(int i=0;i<src_nAttrs;i++){
+        AttrCatEntry attrcatentry;
+        AttrCacheTable::getAttrCatEntry(srcRelid,i,&attrcatentry);
+        strcpy(attr_names[i],attrcatentry.attrName);
+        attr_types[i]=attrcatentry.attrType;       
+    }
+    int ret=Schema::createRel(targetRel,src_nAttrs,attr_names,attr_types);
+    if(ret!=SUCCESS)
+{
+    return ret;
+}
+
+    int targetrelid=OpenRelTable::openRel(targetRel);
+    if(targetrelid<0 || targetrelid>=MAX_OPEN){
+        Schema::deleteRel(targetRel);
+        return targetrelid;
+    }
+
+
+
+
+
+    Attribute record[src_nAttrs];
+
+   
+    RelCacheTable::resetSearchIndex(srcRelid);
+    AttrCacheTable::resetSearchIndex(srcRelid,attr);
+
+    while (BlockAccess::search(srcRelid,record,attr,attrVal,op)==SUCCESS)
+    {
+
+        ret = BlockAccess::insert(targetrelid, record);
+        if(ret!=SUCCESS){
+            Schema::closeRel(targetRel);
+            Schema::deleteRel(targetRel);
+            return ret;
+        }
+       
+    }
+
+    Schema::closeRel(targetRel);
+
+    return SUCCESS;
+}
